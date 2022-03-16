efadab7f9f895c31d61e2fa0643fbd59f8217e67
Kichura <68134602+Kichura@users.noreply.github.com>
2022-03-16 22:04:37 +0100

    Actually, undo the version to when it was before.
    Best to leave this one up to wildfire to decide instead.

--- src/main/java/com/wildfire/main/WildfireGender.java
+++ src/main/java/com/wildfire/main/WildfireGender.java
@@ -57,7 +57,7 @@ import org.lwjgl.glfw.GLFW;
 import javax.annotation.Nullable;
 
 public class WildfireGender implements ClientModInitializer {
-	public static final String VERSION = "2.9";
+	public static final String VERSION = "2.8.2";
   	public static final String MODID = "wildfire_gender";
 	private static KeyBinding keyBinding;
 
