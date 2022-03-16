970b8492e06a1a56fcdf16ee670a7c9330b09b7c
Kichura <68134602+Kichura@users.noreply.github.com>
2022-03-16 21:32:54 +0100

    Bump version to 2.9.
    The "pre" context was marked as inaccurate by wildfire - so i will change it.

--- src/main/java/com/wildfire/main/WildfireGender.java
+++ src/main/java/com/wildfire/main/WildfireGender.java
@@ -57,7 +57,7 @@ import org.lwjgl.glfw.GLFW;
 import javax.annotation.Nullable;
 
 public class WildfireGender implements ClientModInitializer {
-	public static final String VERSION = "2.8.3-pre";
+	public static final String VERSION = "2.9";
   	public static final String MODID = "wildfire_gender";
 	private static KeyBinding keyBinding;
 
