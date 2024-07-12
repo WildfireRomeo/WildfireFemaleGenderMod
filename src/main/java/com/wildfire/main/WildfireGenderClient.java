952acb58d0f23322b5bbc48a2a0c607e98efa458
Sara Freimer <sara@freimer.com>
2024-07-12 11:37:04 -0500

    Fix forgetting to specify the dist

--- src/main/java/com/wildfire/main/WildfireGenderClient.java
+++ src/main/java/com/wildfire/main/WildfireGenderClient.java
@@ -1,11 +1,12 @@
 package com.wildfire.main;
 
 import com.wildfire.main.config.GeneralClientConfig;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig.Type;
 
-@Mod(WildfireGender.MODID)
+@Mod(value = WildfireGender.MODID, dist = Dist.CLIENT)
 public class WildfireGenderClient {
 
     public WildfireGenderClient(ModContainer modContainer) {
